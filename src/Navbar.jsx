f1953faa4d343bf96987b2eefbd946ec70a913c1
kebin20 <ktanzyl@hotmail.com>
2024-02-06 16:44:46 +0900

    feat: created basic appbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,20 +4,35 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Switch from "@mui/material/Switch";
 
-const label = { inputProps: { "aria-label": "Switch demo" } };
+const label = { inputProps: { "aria-label": "Colour theme switcher" } };
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h3" component="h1" sx={{ flexGrow: 1 }}>
+      <AppBar
+        sx={{
+          boxShadow: "none",
+          backgroundColor: "#023047",
+        }}
+        position="static"
+      >
+        <Toolbar
+          sx={{
+            display: "flex",
+            gap: "90px",
+            padding: "40px",
+            marginInline: "10px",
+          }}
+        >
+          <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
             devfinder
           </Typography>
-          <Typography variant="h3" component="h1" sx={{ flexGrow: 1 }}>
-            repofinder
-          </Typography>
-          <Switch {...label} defaultChecked />
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="span" sx={{ flexGrow: 1 }}>
+              LIGHT
+            </Typography>
+            <Switch {...label} defaultChecked />
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
