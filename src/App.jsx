11811e746eba854a9d0d6aad19e47d4e5b498eda
kebin20 <ktanzyl@hotmail.com>
2024-02-07 15:39:03 +0900

    style: added background colour to site

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
-// import UserDetails from "./UserDetails";
+import UserDetails from "./UserDetails";
 
 import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
-    <>
+    <div style={{ backgroundColor: "#151c2eff" }}>
       <CssBaseline />
       <Navbar />
       <SearchBar />
-      {/* <UserDetails /> */}
-    </>
+      <UserDetails />
+    </div>
   );
 }
 
